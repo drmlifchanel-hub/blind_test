e2723438987f8845fe02b5da4ce26a97e3e3d2f4
drmlifchanel-hub <drmlifchanel@gmail.com>
2026-03-27 15:13:46 +0100

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,898 @@
+window.BLINDTEST_QUESTIONS = [
+  {
+    "round": "Manche 1",
+    "number": 1,
+    "label": "Manche 1 • Question 1",
+    "budget": {
+      "1": 1,
+      "0.5": 0,
+      "0.25": 0
+    },
+    "answers": {
+      "1": [
+        "Dj Stump"
+      ],
+      "0.5": [],
+      "0.25": []
+    },
+    "single_auto": true
+  },
+  {
+    "round": "Manche 1",
+    "number": 2,
+    "label": "Manche 1 • Question 2",
+    "budget": {
+      "1": 1,
+      "0.5": 0,
+      "0.25": 1
+    },
+    "answers": {
+      "1": [
+        "Aucun"
+      ],
+      "0.5": [],
+      "0.25": [
+        "Bordélique"
+      ]
+    },
+    "single_auto": false
+  },
+  {
+    "round": "Manche 1",
+    "number": 3,
+    "label": "Manche 1 • Question 3",
+    "budget": {
+      "1": 1,
+      "0.5": 0,
+      "0.25": 0
+    },
+    "answers": {
+      "1": [
+        "Violet"
+      ],
+      "0.5": [],
+      "0.25": []
+    },
+    "single_auto": true
+  },
+  {
+    "round": "Manche 1",
+    "number": 4,
+    "label": "Manche 1 • Question 4",
+    "budget": {
+      "1": 1,
+      "0.5": 0,
+      "0.25": 0
+    },
+    "answers": {
+      "1": [
+        "Les audios (trop longs)"
+      ],
+      "0.5": [],
+      "0.25": []
+    },
+    "single_auto": true
+  },
+  {
+    "round": "Manche 1",
+    "number": 5,
+    "label": "Manche 1 • Question 5",
+    "budget": {
+      "1": 1,
+      "0.5": 0,
+      "0.25": 1
+    },
+    "answers": {
+      "1": [
+        "Avoir tous les gens qu'il aime autour de lui"
+      ],
+      "0.5": [],
+      "0.25": [
+        "Porsche GT3 RS"
+      ]
+    },
+    "single_auto": false
+  },
+  {
+    "round": "Manche 2",
+    "number": 1,
+    "label": "Manche 2 • Question 1",
+    "budget": {
+      "1": 0,
+      "0.5": 2,
+      "0.25": 1
+    },
+    "answers": {
+      "1": [],
+      "0.5": [
+        "Dj Snake",
+        "Turn Down For What"
+      ],
+      "0.25": [
+        "Lil Jon"
+      ]
+    },
+    "single_auto": false
+  },
+  {
+    "round": "Manche 2",
+    "number": 2,
+    "label": "Manche 2 • Question 2",
+    "budget": {
+      "1": 0,
+      "0.5": 2,
+      "0.25": 1
+    },
+    "answers": {
+      "1": [],
+      "0.5": [
+        "David Guetta",
+        "Titanium"
+      ],
+      "0.25": [
+        "Sia"
+      ]
+    },
+    "single_auto": false
+  },
+  {
+    "round": "Manche 2",
+    "number": 3,
+    "label": "Manche 2 • Question 3",
+    "budget": {
+      "1": 0,
+      "0.5": 2,
+      "0.25": 0
+    },
+    "answers": {
+      "1": [],
+      "0.5": [
+        "OMID",
+        "Enola Gay"
+      ],
+      "0.25": []
+    },
+    "single_auto": false
+  },
+  {
+    "round": "Manche 2",
+    "number": 4,
+    "label": "Manche 2 • Question 4",
+    "budget": {
+      "1": 0,
+      "0.5": 2,
+      "0.25": 0
+    },
+    "answers": {
+      "1": [],
+      "0.5": [
+        "M83",
+        "Outro"
+      ],
+      "0.25": []
+    },
+    "single_auto": false
+  },
+  {
+    "round": "Manche 2",
+    "number": 5,
+    "label": "Manche 2 • Question 5",
+    "budget": {
+      "1": 0,
+      "0.5": 2,
+      "0.25": 0
+    },
+    "answers": {
+      "1": [],
+      "0.5": [
+        "Lana Del Rey",
+        "Young & Beautiful"
+      ],
+      "0.25": []
+    },
+    "single_auto": false
+  },
+  {
+    "round": "Manche 2",
+    "number": 6,
+    "label": "Manche 2 • Question 6",
+    "budget": {
+      "1": 0,
+      "0.5": 2,
+      "0.25": 0
+    },
+    "answers": {
+      "1": [],
+      "0.5": [
+        "Michel Delpech",
+        "Le Chasseur"
+      ],
+      "0.25": []
+    },
+    "single_auto": false
+  },
+  {
+    "round": "Manche 2",
+    "number": 7,
+    "label": "Manche 2 • Question 7",
+    "budget": {
+      "1": 0,
+      "0.5": 2,
+      "0.25": 0
+    },
+    "answers": {
+      "1": [],
+      "0.5": [
+        "Avicii",
+        "Levels"
+      ],
+      "0.25": []
+    },
+    "single_auto": false
+  },
+  {
+    "round": "Manche 2",
+    "number": 8,
+    "label": "Manche 2 • Question 8",
+    "budget": {
+      "1": 0,
+      "0.5": 2,
+      "0.25": 0
+    },
+    "answers": {
+      "1": [],
+      "0.5": [
+        "Joe Dassin",
+        "Dans Les Yeux D'Emilie"
+      ],
+      "0.25": []
+    },
+    "single_auto": false
+  },
+  {
+    "round": "Manche 2",
+    "number": 9,
+    "label": "Manche 2 • Question 9",
+    "budget": {
+      "1": 0,
+      "0.5": 2,
+      "0.25": 0
+    },
+    "answers": {
+      "1": [],
+      "0.5": [
+        "Martin Garrix",
+        "Animals"
+      ],
+      "0.25": []
+    },
+    "single_auto": false
+  },
+  {
+    "round": "Manche 2",
+    "number": 10,
+    "label": "Manche 2 • Question 10",
+    "budget": {
+      "1": 0,
+      "0.5": 2,
+      "0.25": 0
+    },
+    "answers": {
+      "1": [],
+      "0.5": [
+        "Ninho",
+        "Un Poco"
+      ],
+      "0.25": []
+    },
+    "single_auto": false
+  },
+  {
+    "round": "Manche 2",
+    "number": 11,
+    "label": "Manche 2 • Question 11",
+    "budget": {
+      "1": 0,
+      "0.5": 2,
+      "0.25": 0
+    },
+    "answers": {
+      "1": [],
+      "0.5": [
+        "Modjo",
+        "Lady"
+      ],
+      "0.25": []
+    },
+    "single_auto": false
+  },
+  {
+    "round": "Manche 2",
+    "number": 12,
+    "label": "Manche 2 • Question 12",
+    "budget": {
+      "1": 0,
+      "0.5": 2,
+      "0.25": 0
+    },
+    "answers": {
+      "1": [],
+      "0.5": [
+        "AC/DC",
+        "Back In Black"
+      ],
+      "0.25": []
+    },
+    "single_auto": false
+  },
+  {
+    "round": "Manche 2",
+    "number": 13,
+    "label": "Manche 2 • Question 13",
+    "budget": {
+      "1": 0,
+      "0.5": 2,
+      "0.25": 0
+    },
+    "answers": {
+      "1": [],
+      "0.5": [
+        "Village People",
+        "YMCA"
+      ],
+      "0.25": []
+    },
+    "single_auto": false
+  },
+  {
+    "round": "Manche 2",
+    "number": 14,
+    "label": "Manche 2 • Question 14",
+    "budget": {
+      "1": 0,
+      "0.5": 2,
+      "0.25": 0
+    },
+    "answers": {
+      "1": [],
+      "0.5": [
+        "Skrillex",
+        "Bangarang"
+      ],
+      "0.25": []
+    },
+    "single_auto": false
+  },
+  {
+    "round": "Manche 2",
+    "number": 15,
+    "label": "Manche 2 • Question 15",
+    "budget": {
+      "1": 0,
+      "0.5": 2,
+      "0.25": 0
+    },
+    "answers": {
+      "1": [],
+      "0.5": [
+        "Benny Benassi",
+        "Satisfaction"
+      ],
+      "0.25": []
+    },
+    "single_auto": false
+  },
+  {
+    "round": "Manche 3",
+    "number": 1,
+    "label": "Manche 3 • Question 1",
+    "budget": {
+      "1": 1,
+      "0.5": 1,
+      "0.25": 0
+    },
+    "answers": {
+      "1": [
+        "Demon Slayer"
+      ],
+      "0.5": [
+        "Saison 3"
+      ],
+      "0.25": []
+    },
+    "single_auto": false
+  },
+  {
+    "round": "Manche 3",
+    "number": 2,
+    "label": "Manche 3 • Question 2",
+    "budget": {
+      "1": 1,
+      "0.5": 0,
+      "0.25": 0
+    },
+    "answers": {
+      "1": [
+        "Friends"
+      ],
+      "0.5": [],
+      "0.25": []
+    },
+    "single_auto": true
+  },
+  {
+    "round": "Manche 3",
+    "number": 3,
+    "label": "Manche 3 • Question 3",
+    "budget": {
+      "1": 1,
+      "0.5": 1,
+      "0.25": 0
+    },
+    "answers": {
+      "1": [
+        "Death Note"
+      ],
+      "0.5": [
+        "Saison 2"
+      ],
+      "0.25": []
+    },
+    "single_auto": false
+  },
+  {
+    "round": "Manche 3",
+    "number": 4,
+    "label": "Manche 3 • Question 4",
+    "budget": {
+      "1": 1,
+      "0.5": 1,
+      "0.25": 0
+    },
+    "answers": {
+      "1": [
+        "Gladiator"
+      ],
+      "0.5": [
+        "Now We Are Free"
+      ],
+      "0.25": []
+    },
+    "single_auto": false
+  },
+  {
+    "round": "Manche 3",
+    "number": 5,
+    "label": "Manche 3 • Question 5",
+    "budget": {
+      "1": 1,
+      "0.5": 1,
+      "0.25": 0
+    },
+    "answers": {
+      "1": [
+        "Pokemon"
+      ],
+      "0.5": [
+        "Saison 1"
+      ],
+      "0.25": []
+    },
+    "single_auto": false
+  },
+  {
+    "round": "Manche 3",
+    "number": 6,
+    "label": "Manche 3 • Question 6",
+    "budget": {
+      "1": 1,
+      "0.5": 2,
+      "0.25": 0
+    },
+    "answers": {
+      "1": [
+        "Transformers"
+      ],
+      "0.5": [
+        "Le Premier",
+        "What I've Done"
+      ],
+      "0.25": []
+    },
+    "single_auto": false
+  },
+  {
+    "round": "Manche 3",
+    "number": 7,
+    "label": "Manche 3 • Question 7",
+    "budget": {
+      "1": 1,
+      "0.5": 0,
+      "0.25": 0
+    },
+    "answers": {
+      "1": [
+        "Kaamelott"
+      ],
+      "0.5": [],
+      "0.25": []
+    },
+    "single_auto": true
+  },
+  {
+    "round": "Manche 3",
+    "number": 8,
+    "label": "Manche 3 • Question 8",
+    "budget": {
+      "1": 1,
+      "0.5": 1,
+      "0.25": 0
+    },
+    "answers": {
+      "1": [
+        "My Hero Academia"
+      ],
+      "0.5": [
+        "Saison 3"
+      ],
+      "0.25": []
+    },
+    "single_auto": false
+  },
+  {
+    "round": "Manche 3",
+    "number": 9,
+    "label": "Manche 3 • Question 9",
+    "budget": {
+      "1": 1,
+      "0.5": 2,
+      "0.25": 0
+    },
+    "answers": {
+      "1": [
+        "Harry Potter"
+      ],
+      "0.5": [
+        "Harry Potter 1",
+        "Hedwig's Theme"
+      ],
+      "0.25": []
+    },
+    "single_auto": false
+  },
+  {
+    "round": "Manche 3",
+    "number": 10,
+    "label": "Manche 3 • Question 10",
+    "budget": {
+      "1": 1,
+      "0.5": 0,
+      "0.25": 0
+    },
+    "answers": {
+      "1": [
+        "Malcolm"
+      ],
+      "0.5": [],
+      "0.25": []
+    },
+    "single_auto": true
+  },
+  {
+    "round": "Manche 3",
+    "number": 11,
+    "label": "Manche 3 • Question 11",
+    "budget": {
+      "1": 1,
+      "0.5": 1,
+      "0.25": 0
+    },
+    "answers": {
+      "1": [
+        "Jujutsu Kaisen"
+      ],
+      "0.5": [
+        "opening 4"
+      ],
+      "0.25": []
+    },
+    "single_auto": false
+  },
+  {
+    "round": "Manche 3",
+    "number": 12,
+    "label": "Manche 3 • Question 12",
+    "budget": {
+      "1": 1,
+      "0.5": 2,
+      "0.25": 0
+    },
+    "answers": {
+      "1": [
+        "Dragon"
+      ],
+      "0.5": [
+        "Dragon 1",
+        "Test Drive"
+      ],
+      "0.25": []
+    },
+    "single_auto": false
+  },
+  {
+    "round": "Manche 3",
+    "number": 13,
+    "label": "Manche 3 • Question 13",
+    "budget": {
+      "1": 1,
+      "0.5": 0,
+      "0.25": 0
+    },
+    "answers": {
+      "1": [
+        "The Big Bang Theory"
+      ],
+      "0.5": [],
+      "0.25": []
+    },
+    "single_auto": true
+  },
+  {
+    "round": "Manche 3",
+    "number": 14,
+    "label": "Manche 3 • Question 14",
+    "budget": {
+      "1": 1,
+      "0.5": 2,
+      "0.25": 1
+    },
+    "answers": {
+      "1": [
+        "Naruto"
+      ],
+      "0.5": [
+        "Naruto Shippuden",
+        "Opening 16"
+      ],
+      "0.25": [
+        "Siluhette"
+      ]
+    },
+    "single_auto": false
+  },
+  {
+    "round": "Manche 3",
+    "number": 15,
+    "label": "Manche 3 • Question 15",
+    "budget": {
+      "1": 1,
+      "0.5": 2,
+      "0.25": 0
+    },
+    "answers": {
+      "1": [
+        "Fast & Furious"
+      ],
+      "0.5": [
+        "Tokyo Drift",
+        "Parell Williams"
+      ],
+      "0.25": []
+    },
+    "single_auto": false
+  },
+  {
+    "round": "Manche 3",
+    "number": 16,
+    "label": "Manche 3 • Question 16",
+    "budget": {
+      "1": 1,
+      "0.5": 0,
+      "0.25": 0
+    },
+    "answers": {
+      "1": [
+        "Ma Famille D'Abord"
+      ],
+      "0.5": [],
+      "0.25": []
+    },
+    "single_auto": true
+  },
+  {
+    "round": "Manche 3",
+    "number": 17,
+    "label": "Manche 3 • Question 17",
+    "budget": {
+      "1": 1,
+      "0.5": 2,
+      "0.25": 1
+    },
+    "answers": {
+      "1": [
+        "L'attaque des titans"
+      ],
+      "0.5": [
+        "Saison 4",
+        "Partie 2"
+      ],
+      "0.25": [
+        "Rambling"
+      ]
+    },
+    "single_auto": false
+  },
+  {
+    "round": "Manche 3",
+    "number": 18,
+    "label": "Manche 3 • Question 18",
+    "budget": {
+      "1": 1,
+      "0.5": 0,
+      "0.25": 0
+    },
+    "answers": {
+      "1": [
+        "Stranger Things"
+      ],
+      "0.5": [],
+      "0.25": []
+    },
+    "single_auto": true
+  },
+  {
+    "round": "Manche 3",
+    "number": 19,
+    "label": "Manche 3 • Question 19",
+    "budget": {
+      "1": 1,
+      "0.5": 0,
+      "0.25": 0
+    },
+    "answers": {
+      "1": [
+        "Avengers"
+      ],
+      "0.5": [],
+      "0.25": []
+    },
+    "single_auto": true
+  },
+  {
+    "round": "Manche 3",
+    "number": 20,
+    "label": "Manche 3 • Question 20",
+    "budget": {
+      "1": 1,
+      "0.5": 1,
+      "0.25": 0
+    },
+    "answers": {
+      "1": [
+        "Bleach"
+      ],
+      "0.5": [
+        "Saison 6"
+      ],
+      "0.25": []
+    },
+    "single_auto": false
+  },
+  {
+    "round": "Manche 4",
+    "number": 1,
+    "label": "Manche 4 • Question 1",
+    "budget": {
+      "1": 1,
+      "0.5": 2,
+      "0.25": 0
+    },
+    "answers": {
+      "1": [
+        "Harry Potter"
+      ],
+      "0.5": [
+        "Harry Potter à l'école des sorciers",
+        "Hermione"
+      ],
+      "0.25": []
+    },
+    "single_auto": false
+  },
+  {
+    "round": "Manche 4",
+    "number": 2,
+    "label": "Manche 4 • Question 2",
+    "budget": {
+      "1": 1,
+      "0.5": 0,
+      "0.25": 0
+    },
+    "answers": {
+      "1": [
+        "Asterix et obelix mission cléopatre"
+      ],
+      "0.5": [],
+      "0.25": []
+    },
+    "single_auto": true
+  },
+  {
+    "round": "Manche 4",
+    "number": 3,
+    "label": "Manche 4 • Question 3",
+    "budget": {
+      "1": 1,
+      "0.5": 0,
+      "0.25": 0
+    },
+    "answers": {
+      "1": [
+        "Ses Clés de voiture"
+      ],
+      "0.5": [],
+      "0.25": []
+    },
+    "single_auto": true
+  },
+  {
+    "round": "Manche 4",
+    "number": 4,
+    "label": "Manche 4 • Question 4",
+    "budget": {
+      "1": 1,
+      "0.5": 0,
+      "0.25": 0
+    },
+    "answers": {
+      "1": [
+        "La Cité de la peur"
+      ],
+      "0.5": [],
+      "0.25": []
+    },
+    "single_auto": true
+  },
+  {
+    "round": "Manche 4",
+    "number": 5,
+    "label": "Manche 4 • Question 5",
+    "budget": {
+      "1": 1,
+      "0.5": 0,
+      "0.25": 2
+    },
+    "answers": {
+      "1": [
+        "Les Voitures"
+      ],
+      "0.5": [],
+      "0.25": [
+        "La Musique",
+        "Le Cinéma"
+      ]
+    },
+    "single_auto": false
+  },
+  {
+    "round": "Manche 4",
+    "number": 6,
+    "label": "Manche 4 • Question 6",
+    "budget": {
+      "1": 1,
+      "0.5": 2,
+      "0.25": 0
+    },
+    "answers": {
+      "1": [
+        "Fast & Furious"
+      ],
+      "0.5": [
+        "2 Fast 2 Furious",
+        "Bryan"
+      ],
+      "0.25": []
+    },
+    "single_auto": false
+  }
+];
